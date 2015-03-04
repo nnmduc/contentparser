bfcad4cbd25dba97648c654f24fec9028c7b39f4
Peter Nguyen <duc.nguyen@puresolutions.com.vn>
2015-03-04 15:46:35 +0700

    add kenh14

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,3 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+require 'uri'
+
 class IndexController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
@@ -5,24 +9,48 @@ class IndexController < ApplicationController
 	end
 
 	def show
-		require 'nokogiri'
-		require 'open-uri'
 		pageUrl = params[:url]
-		doc = Nokogiri::HTML(open(pageUrl))
+		uri = URI(pageUrl)
+		case uri.host
+		when "kienthuc.net.vn"
+			@content = kienthuc(pageUrl)
+		when "kenh14.vn"
+			@content = kenh14(pageUrl)
+		else
+			@content = unknow(pageUrl)
+		end
+	end
 
-		body = doc.css('div#abody')
-		body.xpath('//@style').remove
-		body.css('div').each do |div|
-			if div.content == ''
-				div.remove
-			else
-				img = div.css('img')
-				if img.count == 0
-					div.name = 'p'
-				end
-			end
+
+	private
+		def unknow(pageUrl)
+			doc = Nokogiri::HTML(open(pageUrl))
+			body = doc.css('body')
+			body.xpath('//@style').remove
+			body.inner_html
 		end
 
-		@content = body.inner_html
-	end
+		def kienthuc(pageUrl)
+			doc = Nokogiri::HTML(open(pageUrl))
+			body = doc.css('div#abody')
+			body.xpath('//@style').remove
+			body.css('div').each do |div|
+				if div.content == ''
+					div.remove
+				else
+					img = div.css('img')
+					if img.count == 0
+						div.name = 'p'
+					end
+				end
+			end
+			body.inner_html
+		end
+
+		def kenh14(pageUrl)
+			doc = Nokogiri::HTML(open(pageUrl))
+			body = doc.css('#admWrapsite > div.contentwrapper > div > div > div.maincontent.clearfix > div.postwrapper.clearfix > div:nth-child(1) > div.content > div')
+			body.xpath('//@style').remove
+			body.inner_html
+		end
 end
